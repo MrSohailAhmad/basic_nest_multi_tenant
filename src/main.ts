d2f00cf66aa823df216b012e52ea2db542c4438b
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-08-12 12:23:27 +0500

    change api docs url

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   // Global Filter
   app.useGlobalFilters(new GlobalExceptionFilter());
